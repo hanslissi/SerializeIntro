6fc8c7a928ab7e390c566c6e12ee28881da21057
Johannes Riedmueller <johannesriedmueller@Johanness-MacBook-Pro.local>
2018-09-28 12:19:59 +0200

    changed Date

--- SerializeIntro/src/SaveTester.java
+++ SerializeIntro/src/SaveTester.java
@@ -1,6 +1,7 @@
 
 import java.io.File;
 import java.time.LocalDate;
+import java.time.Month;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,8 +17,8 @@ public class SaveTester {
     
     public static void main(String[] args) {
         SchuelerBL bl = new SchuelerBL();
-        bl.add(new Schueler("Johannes", LocalDate.now()));
-        bl.add(new Schueler("Jonny", LocalDate.now()));
+        bl.add(new Schueler("Johannes", LocalDate.of(2002, Month.JUNE, 8)));
+        bl.add(new Schueler("Jonny", LocalDate.of(2002, Month.AUGUST, 19)));
         try{
         bl.save(new File("./klasse.csv"));
         }
